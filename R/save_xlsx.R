4f2260fbe84ff90d40bd688025e89c3b2c414f93
Joan <Joan.Kant@uhn.ca>
2024-07-18 15:19:26 -0400

    simplify excel summary

--- R/save_xlsx.R
+++ R/save_xlsx.R
@@ -4,6 +4,7 @@
 #' @param alpha significance threshold for filtering p-values (default = 0.05)
 #' @param output_dir output directory for saving output (default = '.')
 #' @param output_name name for output file (default = "interactions_summary")
+#' @param is_stringent only keep stringent interactions (default = FALSE)
 #' @export
 #' @importFrom dplyr %>%
 save_as_xlsx <- function(
@@ -11,7 +12,7 @@ save_as_xlsx <- function(
     condition_var = "Condition_dummy",
     alpha = 0.05,
     output_dir = ".",
-    output_name = "interactions_summary") {
+    output_name = "interactions_summary", is_stringent = FALSE) {
     output_filename <- glue::glue("{output_dir}/{output_name}.xlsx")
 
     if (file.exists(output_filename)) {
@@ -20,20 +21,19 @@ save_as_xlsx <- function(
     message("Load data...")
     obj <- readRDS(interactions_agg_integration)
 
-    message("Filtering...")
-    obj_filtered <- obj %>%
-        filter(!is.na(lenient_condition), lenient_condition, pval < alpha) %>%
-        dplyr::select(
-            !!dplyr::sym(condition_var), source_target, complex_interaction,
-            lenient_condition_n_patients, lenient_condition_n_samples,
-            lenient_condition_samples, lenient_condition_patients,
-            pval,
-            # Interaction scores
-            LIANA_score, CellPhoneDB_score, CellChat_score
-        ) %>%
-        dplyr::distinct() %>%
-        as.data.frame()
-
+    message(glue::glue("Filtering (alpha = {alpha})..."))
+    if (is_stringent) {
+        message("Only keep interactions that passed the 'stringent' filter...")
+        obj_filtered <- obj %>%
+            filter(pval < alpha, stringent_condition) %>%
+            dplyr::distinct() %>%
+            as.data.frame()
+    } else {
+        obj_filtered <- obj %>%
+            filter(pval < alpha) %>%
+            dplyr::distinct() %>%
+            as.data.frame()
+    }
     message("Write data to Excel...")
     xlsx::write.xlsx(
         obj_filtered,
