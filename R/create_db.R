21de0fc94b6974cdd6307d087d929cc14dbecde9
joan-yanqiong <Joan.Kant@uhn.ca>
2024-07-19 00:11:05 -0400

    add revert after running bash script

--- R/create_db.R
+++ R/create_db.R
@@ -62,6 +62,9 @@ create_db <- function(source_cpdb_dir, output_dir) {
     Sys.chmod(run_script, mode = "0555")
     Sys.chmod(python_script, mode = "0555")
     system(glue::glue("{run_script} {python_script} {output_dir}"))
+    # Revert to previous mode
+    Sys.chmod(run_script, mode = "644")
+    Sys.chmod(python_script, mode = "644")
 
     cpdb_file <- list.files(output_dir, pattern = ".zip", full.names = TRUE)[1]
     file.rename(cpdb_file, glue::glue("{output_dir}/cpdb.zip"))
